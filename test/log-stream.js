ecd8ac8bf53374d9d16e70302bbb6715fe5d67b4
Coen Hyde <coen.hyde@gmail.com>
2016-10-24 17:06:34 -0700

    added tests for logstream

---
+++ test/log-stream.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var expect = require('expect.js');
+var AWS = require('aws-sdk-mock');
+
+var LogsStream = require('../lib/log-stream');
+
+describe('LogStream', function() {
+  this.timeout(5000);
+
+  var options = {
+    logGroup: 'yee',
+    logStream: `yo`,
+    endOfStreamIdentifier: 'ohiv09vaepnjpasv'
+  }
+
+  var staticLogs = [
+    { timestamp: 1477346285562, message: 'Weee logs' },
+    { timestamp: 1477346285563, message: 'more logs' },
+    { timestamp: 1477346285565, message: new Buffer(options.endOfStreamIdentifier).toString('base64') }
+  ];
+
+  it('should fetch logs from AWS Cloudwatch ', function(done) {
+    var fetch_count = 0;
+
+    AWS.mock('CloudWatchLogs', 'getLogEvents', function (params, cb){
+      expect(params.logGroupName).to.equal(options.logGroup);
+      expect(params.logStreamName).to.equal(options.logStream);
+
+      var res = {
+        events: [ staticLogs[fetch_count] ]
+      };
+
+      fetch_count++;
+      setTimeout(() => cb(null, res), 200);
+    });
+
+    var stream = new LogsStream(options);
+    var logsCollection = [];
+
+    stream.on('data', (data) => {
+      logsCollection.push(data);
+    });
+
+    stream.on('end', () => {
+      expect(logsCollection).to.eql(staticLogs);
+      done();
+    });
+  });
+
+  after(function() {
+    AWS.restore('CloudWatchLogs', 'getLogEvents');
+  });
+});
